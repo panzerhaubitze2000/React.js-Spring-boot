029e4761e8dd5a9112ccbfd4864cee6c4e615257
panzerhaubitze2000 <108591407+panzerhaubitze2000@users.noreply.github.com>
2022-10-15 21:05:25 +0300

    Add files via upload

---
+++ demo/demo/src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public @ResponseBody String greeting() {
+        return "Java the best";
+    }
+
+}
